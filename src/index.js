279d91916edc1df1367b8b7d865c77f717634ae5
idan <idan@takescoop.com>
2021-01-15 15:11:50 -0800

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
+
+import App from './App'; // import last to ensure CSS from App are loaded with highest precedence 
 
 ReactDOM.render(
   <React.StrictMode>
